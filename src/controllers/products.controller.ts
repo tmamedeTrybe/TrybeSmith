c6f8b27776efa3c2140cfa491227670c50ab5a79
Thiago Mamede <tmamede2@gmail.com>
2022-09-14 13:51:59 -0300

    ADD - Cria validacao para cadastrar produto

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,9 +1,20 @@
 import { Response, Request } from 'express';
 import productsService from '../services/products.services';
 import Iproduct from '../interfaces/products.interface';
+import validateProduct from '../validations/products.validation';
 
-const createProduct = async (req: Request<unknown, unknown, Iproduct>, res: Response<Iproduct>) => {
+const createProduct = async (req: Request<unknown, unknown, Iproduct>, res: Response) => {
   const newProduct = req.body;
+  const { error } = validateProduct(newProduct);
+
+  if (error?.message === '"name" is required' || error?.message === '"amount" is required') {
+    return res.status(400).json({ message: error.message });
+  }
+
+  if (error) {
+    return res.status(422).json({ message: error.message });
+  }
+
   const productCreated = await productsService.createProduct(newProduct);
   return res.status(201).json(productCreated);
 };
---
+++ src/validations/products.validation.ts
@@ -0,0 +1,18 @@
+import Joi from 'joi';
+import Iproduct from '../interfaces/products.interface';
+
+const validateProduct = (productData: Iproduct) =>
+  Joi.object({
+    name: Joi.string().min(2).required().messages({
+      'string.string': '"name" must be a string',
+      'string.min': '"name" length must be at least 3 characters long',
+      'string.required': '"name" is required',
+    }),
+    amount: Joi.string().min(5).required().messages({
+      'string.string': '"amount" must be a string',
+      'string.min': '"amount" length must be at least 3 characters long',
+      'string.required': '"amount" is required',
+    }),
+  }).validate(productData);
+
+export default validateProduct;
